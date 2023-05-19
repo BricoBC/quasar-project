73a9342ebf08f1cd1bdb035c401d54bdc2e23a4b
BricoBC <barajas.c.bruno.n@gmail.com>
2023-05-18 21:32:41 -0500

    first steps

---
+++ quasar-project/src/layouts/MainLayout.vue
@@ -0,0 +1,94 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="las la-bars"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title> Quasar App </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header> Essential Links </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
+
+const linksList = [
+  {
+    title: "Flex Layout",
+    caption: "Estilos con flex",
+    icon: "las la-truck-loading",
+    link: "fle",
+  },
+  {
+    title: "Typography",
+    caption: "Tipos de letra en Quasar",
+    icon: "las la-bold",
+    link: "typo",
+  },
+  {
+    title: "Docs",
+    caption: "quasar.dev",
+    icon: "las la-graduation-cap",
+    link: "https://quasar.dev",
+  },
+  {
+    title: "Github",
+    caption: "github.com/quasarframework",
+    icon: "lab la-github",
+    link: "https://github.com/BricoBC",
+  },
+  {
+    title: "Facebook",
+    caption: "@QuasarFramework",
+    icon: "lab la-facebook",
+    link: "https://www.facebook.com/Brunimonool/",
+  },
+];
+
+export default defineComponent({
+  name: "MainLayout",
+
+  components: {
+    EssentialLink,
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>
---
+++ quasar-project/src/router/routes.js
@@ -0,0 +1,21 @@
+const routes = [
+  {
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      { path: "", component: () => import("pages/IndexPage.vue") },
+      { path: "typography", name: "typo", component: () => import("pages/TypographyPage.vue"), },
+      { path: "flex", name: "fle", component: () => import("pages/FlexPage.vue"), },
+
+    ],
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
+
+export default routes;
